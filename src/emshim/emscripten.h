3ba2995e3ac1a77857a29b884fb74b5235ed0f93
Luis Montes <monteslu@gmail.com>
2026-06-13 02:02:24 -0700

    Core matrix: drop redundant skia archives on macOS (libcanvas.a bundles Skia, Apple ld duplicate-symbol), android API 26 (pthread_getname_np), emshim off_t fix for MSVC

--- src/emshim/emscripten.h
+++ src/emshim/emscripten.h
@@ -1,6 +1,5 @@
 // emscripten.h shim for NATIVE builds of the webaudio-node engine sources.
-// The engine #includes <emscripten.h> only for EMSCRIPTEN_KEEPALIVE and a few
-// POSIX types the WASM toolchain provided implicitly.
+// The engine #includes <emscripten.h> only for EMSCRIPTEN_KEEPALIVE.
 #ifndef JSG_EMSCRIPTEN_SHIM_H
 #define JSG_EMSCRIPTEN_SHIM_H
 
@@ -8,21 +7,17 @@
 #include <cstring>
 #include <cmath>
 #include <string>
+#include <sys/types.h>  // ssize_t / off_t where the engine touches them
 
 #ifdef _MSC_VER
 #define EMSCRIPTEN_KEEPALIVE
 #include <BaseTsd.h>
-#include <sys/types.h>
-typedef SSIZE_T jsg_ssize_t;
 #ifndef _SSIZE_T_DEFINED
 #define _SSIZE_T_DEFINED
 typedef SSIZE_T ssize_t;
 #endif
-// MSVC has no off_t in the bare global namespace; uaac.h's stubs need it.
-typedef int64_t off_t;
 #else
 #define EMSCRIPTEN_KEEPALIVE __attribute__((used))
-#include <sys/types.h>  // ssize_t, off_t
 #endif
 
 #endif  // JSG_EMSCRIPTEN_SHIM_H
